0e278b6dd0e178a3c7e6068c98dea1b2685fdafd
108B32907 <91442753+108B32907@users.noreply.github.com>
2021-12-06 11:03:37 +0800

    Add files via upload

---
+++ Java02.java
@@ -0,0 +1,19 @@
+
+public class Java02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=12,den=0;
+		int ans;
+		if(den!=0) {
+			ans=num/den;
+			System.out.println("ans="+ans);
+		}
+		else {
+			System.out.println("°£¼Æ¬°0");
+		}
+
+		System.out.println("end of main()!!");
+	}
+
+}
